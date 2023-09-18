c6c101995903b1619f2d2a7f0836ee2170330232
Chinwenma <okoriechinwe1327@gmail.com>
2023-09-17 23:57:28 -0800

    ingeration

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Ecommerce</title>
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <?php include('preloader.php'); ?>
+  <?php include('navbar.php'); ?>
+  <main class="main">
